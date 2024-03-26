f4229dfd3d5916ff35d36f6b08f9a96c56366b18
gil gil <83612833+ballestrimg@users.noreply.github.com>
2024-03-26 12:46:35 +0100

    Update graphiques.py
    ajout nouvelles routes : moyenne_loyer_par_region ET moyenne_loyer_par_region_data

--- application/app/routes/graphiques.py
+++ application/app/routes/graphiques.py
@@ -2,23 +2,29 @@ from ..app import app, db
 from flask import render_template, request, flash, jsonify
 from flask_login import login_required
 from ..models.db_citynder import Commune, Etablissements_culturels
-from sqlalchemy import func
+from sqlalchemy.sql import func
 
-@app.route("/graphiques/moyenne_loyer_par_commune", methods=['GET', 'POST'])
-def graphique_loyer_par_commune():
-    return render_template("pages/graphiques/moyenne_loyer_par_commune.html")
+# définition de la route pour afficher le graphique
+@app.route("/graphiques/moyenne_loyer_par_region", methods=['GET', 'POST']) 
+def graphique_loyer_par_region(): 
+    # renvoie le template HTML pour afficher le graphique
+    return render_template("pages/graphiques/moyenne_loyer_par_region.html") 
 
-@app.route('/moyenne_loyer_par_commune_data', methods=['GET', 'POST'])
-def moyenne_loyer_par_commune_data():
-    communes = Commune.query.limit(35)
+# définition de la route pour fournir les données du graphique via une requête
+@app.route('/moyenne_loyer_par_region_data', methods=['GET', 'POST']) 
+def moyenne_loyer_par_region_data():  
+    # récupère les régions et leur moyenne de loyer par mètre carré à partir de la base de données
+    regions = Commune.query.with_entities(Commune.REGION, func.avg((Commune.LOYERM2_APPART + Commune.LOYERM2_MAISON)/2)).group_by(Commune.REGION).all()
+    
+    # initialise une liste vide pour stocker les données à renvoyer
     data = []
-
-    for commune in communes.all():
-        loyer = (commune.LOYERM2_APPART + commune.LOYERM2_MAISON)/2
+    # parcours des régions et de leur moyenne de loyer
+    for region in regions:
+        # ajoute les données de chaque région à la liste
         data.append({
-                'label': commune.LIBGEO,
-                'nombre': loyer
+            'label': region[0], # le nom de la région
+            'nombre': round(region[1], 2), # la moyenne de loyer arrondie à deux décimales
         })
-
-
+    
+    # renvoie les données au format JSON
     return jsonify(data)
