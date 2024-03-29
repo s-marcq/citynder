fc32d0d431205d1c5da5400f63084f05161f86d5
Sarah Marcq <105782832+s-marcq@users.noreply.github.com>
2024-03-30 00:26:02 +0100

    Delete app directory

--- app/statics/heatmap.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('/carte_heatmap')
-        .then(response => response.json())
-        .then(data => {
-            Plotly.newPlot('heatmap-container', data);
-        });
-    });
